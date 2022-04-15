b34ca2ea9182e4cda0a40d3bb0c84f127ffe535f
Arati Langote <67220674+aratilangote@users.noreply.github.com>
2022-04-15 23:25:35 +0530

    Add files via upload

---
+++ StackPop/StackPop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+public class Program
+{
+	public static void Main()
+	{
+		Stack myStack = new Stack();
+		myStack.Push(1);
+		myStack.Push(2);
+		myStack.Push(3);
+		myStack.Push(4);
+
+		Console.WriteLine("Number of elements in Stack: {0}", myStack.Count);
+
+		while (myStack.Count > 0)
+			Console.Write(myStack.Pop() + ",");
+
+		Console.WriteLine();
+		Console.WriteLine("Number of elements in Stack: {0}", myStack.Count);
+	}
+}
