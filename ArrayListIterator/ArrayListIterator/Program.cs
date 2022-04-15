8146b105b8ff6d025dd64c4ea21bfa3323adac9b
Arati Langote <67220674+aratilangote@users.noreply.github.com>
2022-04-15 23:19:01 +0530

    Add files via upload

---
+++ ArrayListIterator/ArrayListIterator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+
+class Program
+{
+        static void Main(string[] args)
+        {
+           ArrayList arlist = new ArrayList()
+                                {
+                                    1,
+                                    "Bill",
+                                    300,
+                                    4.5F
+                                };
+
+                    Console.WriteLine("foreach loop");
+                    foreach (var item in arlist)
+                        Console.Write(item + ", "); //output: 1, Bill, 300, 4.5, 
+
+                    Console.WriteLine("");
+                    Console.WriteLine("for loop");
+
+                    for (int i = 0; i < arlist.Count; i++)
+                        Console.Write(arlist[i] + ", "); //output: 1, Bill, 300, 4.5
+        }
+    
+
+}
+
+
