e5f5c9edf69f6600b84d3af29564fceb5d523c2a
Arati Langote <67220674+aratilangote@users.noreply.github.com>
2022-04-15 23:23:33 +0530

    Add files via upload

---
+++ QueuePop/QueuePop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static void Main()
+	{
+		Queue<string> strQ = new Queue<string>();
+		strQ.Enqueue("H");
+		strQ.Enqueue("e");
+		strQ.Enqueue("l");
+		strQ.Enqueue("l");
+		strQ.Enqueue("o");
+
+		Console.WriteLine("Total elements: {0}", strQ.Count); //prints 5
+
+		if (strQ.Count > 0)
+		{
+			Console.WriteLine(strQ.Peek()); //prints H
+			Console.WriteLine(strQ.Peek()); //prints H
+			Console.WriteLine(strQ.Peek()); //prints H
+		}
+
+		Console.WriteLine("Total elements: {0}", strQ.Count); //prints 0
+	}
+}
