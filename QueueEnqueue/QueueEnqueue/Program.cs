e519c1104915028cf4017332ec18c297843217d3
Arati Langote <67220674+aratilangote@users.noreply.github.com>
2022-04-15 23:22:55 +0530

    Add files via upload

---
+++ QueueEnqueue/QueueEnqueue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static void Main()
+	{
+		Queue<string> strQ = new Queue<string>();
+		strQ.Enqueue("H");
+		strQ.Enqueue("e");
+		strQ.Enqueue("l");
+		strQ.Enqueue("l");
+		strQ.Enqueue("o");
+
+		Console.WriteLine("Total elements: {0}", strQ.Count); //prints 5
+
+		while (strQ.Count > 0)
+			Console.WriteLine(strQ.Dequeue()); //prints Hello
+
+		Console.WriteLine("Total elements: {0}", strQ.Count); //prints 0
+	}
+}
