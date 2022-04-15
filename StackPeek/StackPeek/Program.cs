30788eb4087ca77e1b600d277c63c31279828eb0
Arati Langote <67220674+aratilangote@users.noreply.github.com>
2022-04-15 23:24:57 +0530

    Add files via upload

---
+++ StackPeek/StackPeek/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static void Main()
+	{
+		Stack<int> myStack = new Stack<int>();
+		myStack.Push(1);
+		myStack.Push(2);
+		myStack.Push(3);
+		myStack.Push(4);
+
+		Console.WriteLine("Number of elements in Stack: {0}", myStack.Count);
+
+		if (myStack.Count > 0)
+		{
+			Console.WriteLine(myStack.Peek());
+			Console.WriteLine(myStack.Peek());
+		}
+
+		Console.WriteLine("Number of elements in Stack: {0}", myStack.Count);
+	}
+}
